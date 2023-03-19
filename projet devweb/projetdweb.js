72fdc8944efedfb06e45542708be8b707bee9346
sanondiaye <128326614+sanondiaye@users.noreply.github.com>
2023-03-19 21:58:23 +0000

    Add files via upload

---
+++ projet devweb/projetdweb.js
@@ -0,0 +1,19 @@
+function parcoursco() {
+	const parcoursco = document.querySelector('.scolaire');
+	const cem = document.querySelector('cem');
+	scolaire.classList.toggle('active');
+	cem.classList.toggle('active');
+}
+function parcoursco1() {
+	const parcoursco1 = document.querySelector('.scolaire1');
+	const lyc = document.querySelector('lyc');
+	scolaire1.classList.toggle('active');
+	lyc.classList.toggle('active');
+} 
+function parcoursco2() {
+	const parcoursco2 = document.querySelector('.scolaire2');
+	const elm = document.querySelector('elm');
+	scolaire2.classList.toggle('active');
+	elm.classList.toggle('active');
+
+}
